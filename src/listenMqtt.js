5cdc4e67248e12c48586f67178267ee114c8e933
MạnhG <41892269+manhkhac@users.noreply.github.com>
2022-02-19 00:45:21 +0700

    Update listenMqtt.js

--- src/listenMqtt.js
+++ src/listenMqtt.js
@@ -131,6 +131,8 @@ function listenMqtt(defaultFuncs, api, ctx, globalCallback) {
         }
 
         mqttClient.publish(topic, JSON.stringify(queue), { qos: 1, retain: false });
+        //onbot
+        mqttClient.publish("/foreground_state", JSON.stringify({"foreground": chatOn}), {qos: 1});
 
         var rTimeout = setTimeout(function() {
             mqttClient.end();
